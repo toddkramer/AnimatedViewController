cb217f38f678a876c13485816ed8d9eabc297093
toddkramer <kramermobileapps@gmail.com>
2014-12-26 12:33:27 -0800

    Initial Commit

---
+++ AnimatedViewController/AnimatedViewController.h
@@ -0,0 +1,19 @@
+//
+//  AnimatedViewController.h
+//  AnimatedViewController
+//
+//  Created by Todd Kramer on 12/26/14.
+//  Copyright (c) 2014 Todd Kramer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AnimatedViewController.
+FOUNDATION_EXPORT double AnimatedViewControllerVersionNumber;
+
+//! Project version string for AnimatedViewController.
+FOUNDATION_EXPORT const unsigned char AnimatedViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AnimatedViewController/PublicHeader.h>
+
+
